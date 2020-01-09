400b58a6d249043c0fc0790b885718459d2405e5
Luis Diego <lfdiego7@gmail.com>
2020-01-09 00:56:11 -0500

    update: version by api

--- modular/src/modules/country/service.js
+++ modular/src/modules/country/service.js
@@ -61,6 +61,63 @@ export default class {
     return sortData
   }
 
+  async findAllV2(orderBy) {
+    const data = await repository.findAllV2()
+
+    if (!orderBy) {
+      return data
+    }
+
+    let sortData = []
+
+    if (orderBy === 'iso_DESC') {
+      sortData = data.sort((a, b) => {
+        if (a.iso > b.iso) {
+          return -1
+        } else if (a.iso < b.iso) {
+          return 1
+        }
+        /* istanbul ignore next */
+        return 0
+      })
+    } else if (orderBy === 'iso_ASC') {
+      sortData = data.sort((a, b) => {
+        if (a.iso < b.iso) {
+          return -1
+        } else if (a.iso > b.iso) {
+          return 1
+        }
+        /* istanbul ignore next */
+        return 0
+      })
+    } else if (orderBy === 'name_DESC') {
+      sortData = data.sort((a, b) => {
+        if (a.name > b.name) {
+          return -1
+        } else if (a.name < b.name) {
+          return 1
+        }
+        /* istanbul ignore next */
+        return 0
+      })
+    } else if (orderBy === 'name_ASC') {
+      sortData = data.sort((a, b) => {
+        if (a.name < b.name) {
+          return -1
+        } else if (a.name > b.name) {
+          return 1
+        }
+        /* istanbul ignore next */
+        return 0
+      })
+    } else {
+      /* istanbul ignore next */
+      sortData = data
+    }
+
+    return sortData
+  }
+
   async findByIso(iso) {
     const data = await repository.findByIso(iso)
     const keys = Object.keys(data || {}).length
--- modular/src/utils/api-version.js
+++ modular/src/utils/api-version.js
@@ -1,27 +1,79 @@
-const regex = /^v?(?:\d+)(\.(?:[x*]|\d+)(\.(?:[x*]|\d+)(\.(?:[x*]|\d+))?(?:-[\da-z\-]+(?:\.[\da-z\-]+)*)?(?:\+[\da-z\-]+(?:\.[\da-z\-]+)*)?)?)?$/i
-const clear = version => version.replace(/\./gi, '')
-const cast = version => parseInt(version)
-
-const versions = (versions, options = {}) => {
-  let tuples = []
-
-  if (options.fallbackLatest && options.defaultVersion) {
-    throw new Error(
-      'Can not set options "fallbackLatest" and "defaultVersion" at same time'
-    )
-  }
-
-  for (let v in versions) tuples.push({ version: v, method: versions[v] })
-
-  tuples.sort((a, b) => {
-    const v1 = clear(a.version)
-    const v2 = clear(b.version)
-
-    return cast(v1) > cast(v2) ? 1 : -1
-  })
+const defaultOptions = {
+  requestHeader: 'x-api-version',
+  responseHeader: 'x-api-version',
+  fallbackLatest: false,
+  defaultVersion: null,
 }
 
-versions({
-  '1.3.0': () => console.log('version 1'),
-  '1.0.0': () => console.log('version 2')
-})
+const clear = version => version.replace(/\./gi, '')
+const cast = version => parseInt(version)
+const clearAndCast = version => cast(clear(version))
+
+const findMethod = (requestVersion, tuples, fallbackLatest = false) => {
+  if (requestVersion === null || requestVersion === '*') {
+    return tuples[0]
+  }
+
+  for (let i = 0, t = tuples.length; i < t; i += 1) {
+    const { version } = tuples[i]
+
+    if (requestVersion === version) {
+      return tuples[i]
+    }
+  }
+
+  if (fallbackLatest) {
+    return tuples[0]
+  }
+
+  return null
+}
+
+const versions = (versions, options = {}) => (ctx, next) => {
+  const config = Object.assign(defaultOptions, options)
+  const tuples = []
+
+  /* istanbul ignore if */
+  if (config.fallbackLatest && config.defaultVersion) {
+    ctx.throw(
+      400,
+      'Can not set options "fallbackLatest" and "defaultVersion" at same time'
+    )
+    return
+  }
+
+  const keys = Object.keys(versions)
+
+  keys.forEach(v => {
+    tuples.push({ version: v, method: versions[v] })
+  })
+
+  tuples.sort((a, b) => {
+    const v1 = clearAndCast(a.version)
+    const v2 = clearAndCast(b.version)
+
+    return v1 < v2 ? 1 : -1
+  })
+
+  try {
+    let version = ctx.get(config.requestHeader) || null
+
+    if (!version && config.defaultVersion) version = config.defaultVersion
+
+    const found = findMethod(version, tuples, config.fallbackLatest)
+
+    if (found) {
+      const { version, method } = found
+      ctx.state.apiVersion = version
+      ctx.set(config.responseHeader, version)
+
+      return method(ctx, next)
+    }
+
+    ctx.throw(400, 'Version ' + version + ' is not supported')
+  } catch (err) {
+    ctx.throw(400, err.message)
+  }
+}
+
+export default versions
