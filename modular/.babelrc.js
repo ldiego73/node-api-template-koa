f05525a41fa146ca6fbce27b805bd26dada8d50f
Luis Diego <lfdiego7@gmail.com>
2020-04-21 19:24:25 -0500

    fix: remove modular babel decorators

--- modular/.babelrc.js
+++ modular/.babelrc.js
@@ -12,13 +12,7 @@ const presets = [
 
 const plugins = [
   '@babel/plugin-proposal-export-default-from',
-  '@babel/plugin-proposal-export-namespace-from',
-  [
-    '@babel/plugin-proposal-decorators',
-    {
-      legacy: true,
-    },
-  ],
+  '@babel/plugin-proposal-export-namespace-from'
 ]
 
 module.exports = { presets, plugins }
